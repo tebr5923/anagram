cec13afa004dd4b92e20ec4875a30a3358c3193c
Dmitry Tebryaev <tebr5923@mail.ru>
2020-05-06 19:19:19 +0700

    rename reverseLetters in collectLetters

--- src/main/java/com/tebr/anagrams/Anagramm.java
+++ src/main/java/com/tebr/anagrams/Anagramm.java
@@ -19,11 +19,9 @@ public class Anagramm {
     }
 
     private String reverseWord(String word) {
-        int length = word.length();
-        Stack<Character> letters = reverseLetters(word);
-
+        Stack<Character> letters = collectLetters(word);
         char[] chars = word.toCharArray();
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < word.length(); i++) {
             if (Character.isLetter(chars[i])) {
                 chars[i] = letters.pop();
             }
@@ -31,13 +29,14 @@ public class Anagramm {
         return new String(chars);
     }
 
-    private Stack<Character> reverseLetters(String word) {
+    private Stack<Character> collectLetters(String word) {
         Stack<Character> letters = new Stack<>();
         for (char ch : word.toCharArray()) {
             if (Character.isLetter(ch)) {
                 letters.push(ch);
             }
         }
+
         return letters;
     }
 }
