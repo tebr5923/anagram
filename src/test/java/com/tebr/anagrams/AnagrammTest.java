3d28df32db135497df0591de1c36f73b4f2cbac0
Dmitry Tebryaev <tebr5923@mail.ru>
2020-05-07 19:36:02 +0700

    rename test after merge

--- src/test/java/com/tebr/anagrams/AnagrammTest.java
+++ src/test/java/com/tebr/anagrams/AnagrammTest.java
@@ -6,20 +6,20 @@ class AnagrammTest {
 
     @org.junit.jupiter.api.Test
     void reversed_ReverseTheText_ReturnReversedText() {
-        Anagramm anagramm = new Anagramm("a123b c4d5t6f7");
+        Anagram anagramm = new Anagram("a123b c4d5t6f7");
         String expResult = "b123a f4t5d6c7";
 
-        String result = anagramm.reversed();
+        String result = anagramm.reverse();
 
         assertEquals(expResult, result);
     }
 
     @org.junit.jupiter.api.Test
     void reversed_ReverseEmptyText_ReturnEmptyText() {
-        Anagramm anagramm = new Anagramm("");
+        Anagram anagramm = new Anagram("");
         String expResult = "";
 
-        String result = anagramm.reversed();
+        String result = anagramm.reverse();
 
         assertEquals(expResult, result);
     }
