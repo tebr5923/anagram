2c0457e08bb7321a803a219883df8db022e0c0c9
Dmitry Tebryaev <tebr5923@mail.ru>
2020-05-07 14:41:05 +0700

    rename reversed_ReverseEmptyText_ReturnReversedText() to reversed_ReverseTheText_ReturnReversedText()

--- src/test/java/com/tebr/anagrams/AnagrammTest.java
+++ src/test/java/com/tebr/anagrams/AnagrammTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class AnagrammTest {
 
     @org.junit.jupiter.api.Test
-    void reversed_ReverseEmptyText_ReturnReversedText() {
+    void reversed_ReverseTheText_ReturnReversedText() {
         Anagramm anagramm = new Anagramm("a123b c4d5t6f7");
         String expResult = "b123a f4t5d6c7";
 
