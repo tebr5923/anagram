a9985fbc3e8a25e288ddd539b25ce0735b0c1ce0
Dmitry Tebryaev <tebr5923@mail.ru>
2020-05-08 13:41:40 +0700

    rename method

--- src/test/java/com/tebr/anagrams/LettersAnagrammTest.java
+++ src/test/java/com/tebr/anagrams/LettersAnagrammTest.java
@@ -29,7 +29,7 @@ class LettersAnagrammTest {
     }
 
     @Test
-    void reversed_ReverseNull_ReturnNullPointerException() {
+    void reversed_ReverseNull_ThrowsNullPointerException() {
         LettersAnagram anagramm = new LettersAnagram(null);
 
         assertThrows(NullPointerException.class, anagramm::reverse);
