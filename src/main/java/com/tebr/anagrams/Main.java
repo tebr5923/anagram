328866a9a77cdb857ee6fd3e8a7c9afc0fe395fa
Dmitry Tebryaev <tebr5923@mail.ru>
2020-05-08 13:12:44 +0700

    rename Anagram.java to LettersAnagram.java

--- src/main/java/com/tebr/anagrams/Main.java
+++ src/main/java/com/tebr/anagrams/Main.java
@@ -3,7 +3,7 @@ package com.tebr.anagrams;
 public class Main {
     public static void main(String[] args) {
         String string = "a1b2c3d4 iuh!@er e5f6g7 h8i9";
-        CachedReversibleString anagram = new CachedReversibleString(new Anagram(string));
+        CachedReversibleString anagram = new CachedReversibleString(new LettersAnagram(string));
         System.out.println(string);
         System.out.println(anagram.reverse());
     }
