db9b7b2b6ca5298c879cbbaa04c4d418d70ad204
Dmitry Tebryaev <tebr5923@mail.ru>
2020-05-09 13:38:49 +0700

    add failed test @Mock

--- src/test/java/com/tebr/anagrams/CachedReversibleStringTest.java
+++ src/test/java/com/tebr/anagrams/CachedReversibleStringTest.java
@@ -1,10 +1,28 @@
 package com.tebr.anagrams;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class CachedReversibleStringTest {
+    @Mock
+    private LettersAnagram lettersAnagram;
+
+    @Test
+    void reverse_shouldCallReverseOneTime_whenCallReverseTwoTimes() {
+        CachedReversibleString cachedReversibleString =
+                new CachedReversibleString(lettersAnagram);
+
+        cachedReversibleString.reverse();
+        cachedReversibleString.reverse();
+
+        verify(lettersAnagram).reverse();
+    }
 
     @Test
     void reverse_LettersAnagramReverseAndCachedReversibleStringReverseIsEquals_ReturnTrue() {
