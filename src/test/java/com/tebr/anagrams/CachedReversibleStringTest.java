bf2c99b8a9ff4b029dd42500871facbc8e2aed05
Dmitry Tebryaev <tebr5923@mail.ru>
2020-05-17 11:35:52 +0700

    rename test method

--- src/test/java/com/tebr/anagrams/CachedReversibleStringTest.java
+++ src/test/java/com/tebr/anagrams/CachedReversibleStringTest.java
@@ -35,7 +35,7 @@ class CachedReversibleStringTest {
     }
 
     @Test
-    void reverse_shouldNotAffectTheResultCachedReversibleString() {
+    void reverse_shouldNotAffectTheResult() {
         String expectedResult = mockReversibleString.reverse();
 
         String result = cachedReversibleString.reverse();
