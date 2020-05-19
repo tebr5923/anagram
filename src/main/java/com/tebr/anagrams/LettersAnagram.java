475e267988be2b0cdbb8f61cad3f05b5445154ac
Dmitry Tebryaev <tebr5923@mail.ru>
2020-05-19 23:17:31 +0700

    use interface

--- src/main/java/com/tebr/anagrams/LettersAnagram.java
+++ src/main/java/com/tebr/anagrams/LettersAnagram.java
@@ -1,6 +1,7 @@
 package com.tebr.anagrams;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringJoiner;
 
 public class LettersAnagram implements ReversibleString {
@@ -24,7 +25,7 @@ public class LettersAnagram implements ReversibleString {
     }
 
     private String reverseWord(String word) {
-        ArrayDeque<Character>  letters = collectLetters(word);
+        Deque<Character>  letters = collectLetters(word);
         char[] chars = word.toCharArray();
         for (int i = 0; i < word.length(); i++) {
             if (Character.isLetter(chars[i])) {
@@ -34,8 +35,8 @@ public class LettersAnagram implements ReversibleString {
         return new String(chars);
     }
 
-    private ArrayDeque<Character> collectLetters(String word) {
-        ArrayDeque<Character>  letters = new ArrayDeque<>();
+    private Deque<Character> collectLetters(String word) {
+        Deque<Character> letters = new ArrayDeque<>();
         for (char ch : word.toCharArray()) {
             if (Character.isLetter(ch)) {
                 letters.push(ch);
